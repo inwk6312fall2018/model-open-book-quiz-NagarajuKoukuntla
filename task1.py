d3944741dca19fe3277166af31d4eabc79eb7e86
NagarajuKoukuntla <43645065+NagarajuKoukuntla@users.noreply.github.com>
2018-10-10 12:15:09 -0300

    Update task1.py
    removed some unnecessary comments and added new one

--- task1.py
+++ task1.py
@@ -47,7 +47,6 @@ print(PT.draw_cards(5))
 
 #-------------------------------second way also tried from other source------
 
-#!/usr/bin/env python3
 import collections
 import itertools
 import random
@@ -65,7 +64,7 @@ class card:
     def __repr__(self):
         return self.numeral + "-" + self.suit
 
-class poker_hand():
+class poker_hand(): #class for checking types 
     def __init__(self, card_list):
         self.card_list = card_list
     def __repr__(self):
