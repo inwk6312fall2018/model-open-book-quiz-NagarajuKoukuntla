655d968d52de9d3ab59af151a911a0049f19f2dc
NagarajuKoukuntla <43645065+NagarajuKoukuntla@users.noreply.github.com>
2018-10-10 12:42:54 -0300

    Update task1.py
    small change in for loop

--- task1.py
+++ task1.py
@@ -10,7 +10,7 @@ def initial_deck_cards():
 		numbers= {'1':'One','2':'Two','3':'Three','4':'Four','5':'Five','6':'Six','7':'Seven','8':'Eight','9':'Nine','10':'Ten','J':'Jack','Q':'Queen','K':'King'}
 
 		for item in suit:#looping and appending
-			for numbers,name in numbers.items():
+			for number,name in numbers.items():
 				cards.append((item,name))
 		print('Generated cards for the table')
 
